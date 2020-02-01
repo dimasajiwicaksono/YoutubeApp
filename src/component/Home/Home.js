6d7f3b883887a084baee1d43df0b78b267d64ea5
dimasajiwicaksono <dimasajiwicaksono@student.uns.ac.idclear>
2020-02-01 10:43:57 +0700

    first commit

---
+++ src/component/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from '../NavBar/Header'
+
+export class Home extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
+            </div>
+        )
+    }
+}
+
+export default Home
